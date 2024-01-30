ef72483790bdd752b6b6d4eadc169548b9b203a8
ruban <ceeeerubankumark513@gmail.com>
2024-01-30 17:11:20 +0530

    redirect

--- hello_flask.py
+++ hello_flask.py
@@ -1,10 +1,23 @@
 from flask import Flask,render_template
 app = Flask(__name__)
+
+
 @app.route("/")
 def index():
     return "Flask Working fine ---ruban"
+
+
 @app.route("/login")
 def template():
     return render_template("index.html")
+
+@app.route("/pageone")
+def pageone():
+    return render_template("firstpage.html")
+ 
+@app.route("/pagetwo")
+def pagetwo():
+    return render_template("secondpage.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
