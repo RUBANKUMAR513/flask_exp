5090d8866aded5b50141cbcbe035d21b43a225d5
ruban <ceeeerubankumark513@gmail.com>
2024-01-31 11:32:29 +0530

    indirect

--- hello_flask.py
+++ hello_flask.py
@@ -1,23 +1,29 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect
+import datetime
 app = Flask(__name__)
-
-
 @app.route("/")
 def index():
-    return "Flask Working fine ---ruban"
-
-
+    return "Flask Working fine -- Ruban"
 @app.route("/login")
 def template():
     return render_template("index.html")
-
 @app.route("/pageone")
 def pageone():
     return render_template("firstpage.html")
- 
 @app.route("/pagetwo")
 def pagetwo():
     return render_template("secondpage.html")
-
+@app.route("/frontpage")
+def frontpage():
+    return render_template("frontpage.html")
+@app.route("/resultpage")
+def resultpage():
+    return render_template("resultpage.html")
+@app.route("/printtime")
+def printtime():
+    print()
+    print(datetime.datetime.now())
+    print()
+    return redirect("/resultpage")
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
