a31267dfc17ea14d308376c4ea9900af5dcde66a
ruban <ceeeerubankumark513@gmail.com>
2024-02-01 16:56:50 +0530

    sql

--- hello_flask.py
+++ hello_flask.py
@@ -1,7 +1,12 @@
 from flask import Flask,render_template,redirect,request
 import datetime
+import sqlite3
 app = Flask(__name__)
 
+Insertquery='INSERT INTO DATA(name,age) VALUES("%s","%s")';
+Fetchquery='SELECT * from data'
+
+dbfilename = "flaskExpDB.db"
 
 @app.route("/")
 def index():
@@ -60,6 +65,46 @@ def statuspage():
 def templateworks():
     d=['srinithin','anand','prasnth']
     return render_template("templateworks.html",data=d)
+
+@app.route("/createpost")
+def createpost():
+    return render_template("createpost.html")
+    
+@app.route("/page_2",methods=["post"])
+def page_2():
+    name=request.form.get("name1")
+    age=request.form.get("number1")
+    conn=sqlite3.connect(dbfilename)
+    cursor=conn.cursor()
+    cursor.execute(Insertquery%(name,age))
+    conn.commit()
+    conn.close()
+    return render_template("page_2.html",name=name,age=age)
+    
+def Createtable():
+    Createtablequery="""CREATE TABLE IF NOT EXISTS "data" (
+                "name" TEXT NOT NULL,
+                "age" TEXT NOT NULL,
+                "_id" INTEGER NOT NULL, PRIMARY KEY("_id" AUTOINCREMENT)
+                );
+                """
+   
+    conn=sqlite3.connect(dbfilename)
+    cursor=conn.cursor()
+    cursor.execute(Createtablequery)
+    conn.commit()
+    conn.close()
+    
+@app.route("/view")
+def fetchall():
+    conn=sqlite3.connect(dbfilename)
+    cursor=conn.cursor()
+    cursor.execute(Fetchquery)
+    receivedData=cursor.fetchall()
+    return render_template("view.html",Result=receivedData)
+    
     
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",debug=True)
+    
+    Createtable()
+    app.run(host="0.0.0.0",port=5050,debug=True)
